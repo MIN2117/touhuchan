6be2e1566b13389b564bfe3803e7886f4940e258
MIN2117 <122804569+MIN2117@users.noreply.github.com>
2023-01-17 04:13:03 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,15 @@
-
-//音
 var audio = new Audio("music/shot.wav");
 var BGM = new Audio("music/bgm.mp3");
 BGM.volume = 0.5;
 BGM.loop = true;
 
-//ゲームスピード
 const GAME_SPEED = 1000/60;
 
-//スクリーンサイズ
 const SCREEN_W = 800;
 const SCREEN_H = 600;
 
-//星の数
 const STAR_MAX = 200;
 
-//キャンバス
 let can = document.getElementById("can");
 let con = can.getContext("2d");
 can.width = SCREEN_W;
@@ -33,7 +27,6 @@ let gameover = false;
 let gamestart = false;
 
 
-//ゲーム読み込み
 function gameStart(){
     for(let i=0;i<STAR_MAX;i++)star[i]=new Star();
     setInterval(gameLoop,GAME_SPEED);
@@ -44,7 +37,6 @@ function gameStart(){
 
 
 
-//スタート
 function start(){
     if(gamestart){
         gameover = false;
@@ -54,24 +46,19 @@ function start(){
     }
 }
 
-//星
 let star = [];
 
-//キーボード
 let keyb = {};
 
-//オブジェクト
 let dorodango = []
 let daizu = [];
 let touhuchan = new Touhuchan();
 daizu[0] = new Daizu( 6, 200<<8,200<<8, 0,0);
 
-//スプライト読み込み
 let spriteImage = new Image();
 spriteImage.src = "png/touhuchan.png";
 
 
-//オブジェクトをアップデート
 function updateObj (obj ){
     for(let i=obj.length-1;i>=0;i--){
         obj[i].update();
@@ -79,19 +66,16 @@ function updateObj (obj ){
     }
 }
 
-//オブジェクトを描画
 function drawObj( obj ){
     for(let i=0;i<obj.length;i++)obj[i].draw();
 }
 
-//移動の処理
 function updateAll(){
     updateObj(daizu);
     updateObj(dorodango);
     touhuchan.update();
 }
 
-//描画の処理
 function drawAll(){
     con.fillStyle ="black";
     con.fillRect(0,0,SCREEN_W,SCREEN_H);
@@ -103,7 +87,6 @@ function drawAll(){
     con.drawImage(can,0,0,SCREEN_W,SCREEN_H,0,0,SCREEN_W,SCREEN_H);
 }
 
-//情報の表示
 function putInfo(){
     con.fillStyle = "white";
 
@@ -133,7 +116,6 @@ function putInfo(){
     con.fillText("Time  :"+timer, 200,40);
 }
 
-//タイマー
 function Settimer(){
     if(!gameover)time += 1;
     if(time >= 30 && timer > 0){timer -= 1;time -= 30;}
@@ -141,9 +123,7 @@ function Settimer(){
     if(!gameover && timer <= 0){timeUp = true; gameover = true;}
 }
 
-//ゲームループ
 function gameLoop(){
-//泥団子を出す
     if(!gameover && dorodango.length < 6)
     dorodango.push( new Dorodango(2,random(40,SCREEN_W-40)<<8,0, 0, 600));
 
@@ -152,17 +132,14 @@ function gameLoop(){
     putInfo();
     Settimer();
 
-//スタートキー
     if(gameover && keyb.Start){
         start();
         keyb.Start = false;
     }
 
-//bgmストップ
     if(gameover)BGM.currentTime = 0;
 }
 
-//ゲーム開始
 window.onload=function(){
     gameStart();
 }
